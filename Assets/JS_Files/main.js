b1235fa86834c98da15c7c4e57c3ed0d6dd3079f
Bradybaldwin <bradybaldwin97@gmail.com>
2021-08-24 01:01:55 -0400

    Added a complete Capstone Project Page

--- Assets/JS_Files/main.js
+++ Assets/JS_Files/main.js
@@ -16,18 +16,31 @@ function progShow() {
     }
 }
 
+//PP Proj Page Link Buttons//
+
+function mouseInLink(obj) {
+    obj.style = 'background-color: #4A9FBA; cursor: pointer;'
+}
+
+function mouseOutLink(obj) {
+    obj.style = 'background-color: white;'
+}
+
+function mouseClickLink1() {
+    window.open('https://pandemic-professors-mhci.webflow.io/')
+}
+
+function mouseClickLink2() {
+    window.open('https://mhci-pandemicprofs-contributor.medium.com/')
+}
+
+
+
 //Animation functions (via Greensock.com)//
 //this makes sure page is loaded before running the animations//
 document.addEventListener("DOMContentLoaded", function(){
 //Home page scrolling animations//
-        var tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.homeScroll1',
-        }});
-        tl.addLabel("start")
-            .from('.homeScroll1', {y:100, opacity: 0, duration: 1.5})
-            .to('.homeScroll1', {y:0, opacity: 100, duration: 1.5})
-
+     
         var tl = gsap.timeline({
             scrollTrigger: {
                 trigger: '.homeScroll2',
@@ -66,6 +79,8 @@ document.addEventListener("DOMContentLoaded", function(){
     var tlHome = gsap.timeline({});
         tlHome.from("#tagline", {x: -100, opacity:0, delay: .3, duration: .7});
         tlHome.from(".homeMain2", {y: 100, opacity:0, duration: .5});
+        tlHome.from("#featWork", {y: 100, opacity:0, duration: .5});
+        tlHome.from(".homeScroll1", {y: 100, opacity:0, duration: .5});
     
 //Non-scroll on load Projects Page timeline//
     var tlProj = gsap.timeline({});
@@ -94,6 +109,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var tlCP = gsap.timeline({});
         tlCP.from("#PPHeadsub", {opacity:0, duration: .5, delay: .3});
         tlCP.from("#PPOvrAnim", {x: 100, opacity:0, duration: .5});
+        tlCP.from(".PPLinks", {x: 100, opacity:0, duration: .5});
         tlCP.from("#PPProjGoalsAnim", {opacity:0, duration: .5});
         tlCP.from("#PPcollabContAnim", {opacity:0, duration: .5});
 
